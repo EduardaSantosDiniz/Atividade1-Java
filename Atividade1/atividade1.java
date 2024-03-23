1d96ff6796b7cf4e29b8f4f95fbddd17ebe846d1
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2024-03-23 10:03:37 -0300

    finalizando tarefa

--- Atividade1/atividade1.java
+++ Atividade1/atividade1.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 
 public class atividade1 {
 
-   
+    public static double somar(double numA,double numB){
+        return numA + numB;
+    }
+
+    public static double subtrair(double numA,double numB){
+        return numA - numB;
     }
 
     public static double multiplicar(double numA,double numB){
